2d47d85b226a0ecf2c9594f7928f6c91f26e3be7
Savannah C <sgcorrell@gmail.com>
2019-07-29 15:50:43 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    if @post.valid?
-      @post.update(post_params)
+    if @post.update(post_params)
       redirect_to post_path(@post)
     else
       render :edit
